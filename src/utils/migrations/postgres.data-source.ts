edbf1b4fbe37d523fdf54db2a135443a0d40f2a3
Elaboro <kviynt@mail.ru>
2023-04-18 21:32:56 +0300

    Change migrations script

---
+++ src/utils/migrations/postgres.data-source.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+
+dotenv.config({ path: `.${process.env.NODE_ENV}.env` });
+
+// settings from src/app.module.ts 
+const data_source = new DataSource({
+  type: "postgres",
+  host: process.env.APP_POSTGRES_HOST,
+  port: Number(process.env.APP_POSTGRES_PORT),
+  username: process.env.APP_POSTGRES_USERNAME,
+  password: process.env.APP_POSTGRES_PASSWORD,
+  database: process.env.APP_POSTGRES_DATABASE,
+  synchronize: false,
+  subscribers: ["src/subscriber/*.{ts, js}"],
+  migrations: ["src/migration/*.{ts, js}"],
+  logging: (process.env.APP_POSTGRES_LOGGING?.toLowerCase() === "true"),
+});
+
+export default data_source;
